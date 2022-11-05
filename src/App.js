718f044d5f0c983b4579b6b7b0521941f3d8cddc
Safaid Ansari <safaidansari07@gmail.com>
2022-11-05 17:39:21 +0530

    Challenge Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Card } from "react-bootstrap";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  async function fetchData() {
+    let result = await fetch("https://jsonplaceholder.typicode.com/users/");
+
+    result = await result.json();
+    console.log(result);
+    setData(result);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {data.map((user, index) => {
+        return (
+          <div className="box">
+            <Card style={{ width: "18rem" }} className="card">
+              <Card.Img variant="top" src="https://picsum.photos/200" />
+              <Card.Body>
+                <Card.Title>{user.name}</Card.Title>
+                <Card.Text>{user.username}</Card.Text>
+                <Button variant="primary ">
+                  <Card.Link
+                    style={{
+                      color: "white",
+                      fontSize: "20px",
+                      textDecoration: "none",
+                    }}
+                    href={`https://jsonplaceholder.typicode.com/users/${user.id}/posts`}
+                  >
+                    Post
+                  </Card.Link>
+                </Button>
+                <Button variant="primary" style={{ margin: "10px" }}>
+                  <Card.Link
+                    style={{
+                      color: "white",
+                      fontSize: "20px",
+                      textDecoration: "none",
+                    }}
+                    href={`https://jsonplaceholder.typicode.com/posts/${user.id}/comments `}
+                  >
+                    Todo
+                  </Card.Link>
+                </Button>
+                <Button variant="primary">
+                  <Card.Link
+                    style={{
+                      color: "white",
+                      fontSize: "20px",
+                      textDecoration: "none",
+                    }}
+                    href={`https://jsonplaceholder.typicode.com/users/${user.id}/albums`}
+                  >
+                    Album
+                  </Card.Link>
+                </Button>
+              </Card.Body>
+            </Card>
+          </div>
+        );
+      })}
+    </>
   );
 }
 
